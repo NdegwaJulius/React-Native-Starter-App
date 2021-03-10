f5909b924c718e6eb21d0939f829d3399ca6f98d
NdegwaJM <juliusndegwa2@gmail.com>
2021-03-10 08:03:47 +0300

    Simple text/password input validation using ternary expression

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,18 +3,18 @@ import {View,Text,TextInput,StyleSheet} from 'react-native';
 
 
 const TextScreen =() => {
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
     return (
         <View>
-            <Text>Enter name:</Text>
+            <Text>Enter Password:</Text>
            <TextInput 
            style ={styles.input}
            autoCapitalize ="none"
            autoCorrect={false}
-           value={name}
-           onChangeText ={(newValue)=>setName(newValue) }
+           value={password}
+           onChangeText ={(newValue)=>setPassword(newValue) }
            />
-           <Text>Your name is {name}</Text>
+           {password.length <8 ? <Text>Password must be at least  8 characters</Text> : null}
         </View>
     );
 
